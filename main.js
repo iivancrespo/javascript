9b205967ed7c206bf62040d5b29359468d8ad127
iivancrespo <88992338+iivancrespo@users.noreply.github.com>
2021-08-24 12:06:38 -0300

    Add files via upload
    primera practica javascript - Ivan Crespo

---
+++ main.js
@@ -0,0 +1,9 @@
+/*Practica de javascript*/
+
+let entrada = prompt("Ingresar tu nombre completo");
+let salida = "Bienvenido" + " " + entrada;
+alert(salida);
+
+let mensajeDeBienvenida = "Gracias por dejarnos tu nombre, ya eres bienvenido" + " " + entrada;
+
+console.log(mensajeDeBienvenida);
