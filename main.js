40b8afc5d18ce2e8b64ff7af41e74d46995680fb
iivancrespo <88992338+iivancrespo@users.noreply.github.com>
2021-08-31 15:46:50 -0300

    2DA CLASE JAVASCRIPT

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*Practica de javascript*/
+/*Practica de javascript
 
 let entrada = prompt("Ingresar tu nombre completo");
 let salida = "Bienvenido" + " " + entrada;
@@ -6,4 +6,25 @@ alert(salida);
 
 let mensajeDeBienvenida = "Gracias por dejarnos tu nombre, ya eres bienvenido" + " " + entrada;
 
-console.log(mensajeDeBienvenida);
+console.log(mensajeDeBienvenida); */
+
+/* segunda clase javascrip */
+
+let altura = prompt("Ingresar altura en 3 digitos y sin comas");
+let edad = prompt("Ingresa tu edad");
+
+if (altura < 120) {
+    alert("Sos un minion de " + edad + " años.");
+}
+else if (altura < 160) {
+    alert("Sos un petiso/a de " + edad + " años.");
+}
+else if (altura < 180) {
+    alert("Tu altura es promedio");
+}
+else if (altura <= 189) {
+    alert("Sos un gigante de " + edad + " años.");
+}
+else if (altura => 190) {
+    alert("Agachate que pasa un avion");
+}
