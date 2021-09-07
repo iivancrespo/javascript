90a226bb2ee288bf97424a9f52dd6512a7777bd0
iivancrespo <88992338+iivancrespo@users.noreply.github.com>
2021-09-07 14:13:46 -0300

    3era clase, Ciclos

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ console.log(mensajeDeBienvenida); */
 
 /* segunda clase javascrip */
 
-let altura = prompt("Ingresar altura en 3 digitos y sin comas");
+/* let altura = prompt("Ingresar altura en 3 digitos y sin comas");
 let edad = prompt("Ingresa tu edad");
 
 if (altura < 120) {
@@ -27,4 +27,43 @@ else if (altura <= 189) {
 }
 else if (altura => 190) {
     alert("Agachate que pasa un avion");
+} */
+
+/* TERCERA CLASE - CICLOS */
+
+let gusto = prompt("Ingresar un gusto de Ginevré para saber el precio");
+let cantidad = prompt("Ingresar cantidad de botellas de 600ml")
+let precioDry = "1000";
+let precioBlue = "1000";
+let precioRosado = "1000";
+let precioGold = "1300";
+let precioCitric = "1200";
+//Repetimos hasta que se ingresa "ESC"
+while (gusto != "ESC") {
+    switch (gusto) {
+        case "dry":
+            alert("El precio total seria " + precioDry * cantidad);
+            break;
+        case "blue":
+            alert("El precio total seria " + precioBlue * cantidad);
+            break;
+        case "rosado":
+            alert("El precio total seria " + precioRosado * cantidad);
+            break;
+        case "citric":
+            alert("El precio total seria " + precioCitric * cantidad);
+            break;
+        case "gold":
+            alert("El precio total seria " + precioGold * cantidad);
+            break;
+        default:
+            alert("Ese no es un gusto de Ginevré, intentalo de vuelta.");
+            break;
+
+            
+    }
+    gusto = prompt("Ingresar un gusto de Ginevré para saber el precio");
+    cantidad = prompt("Ingresar cantidad de botellas de 600ml");
+
+    
 }
